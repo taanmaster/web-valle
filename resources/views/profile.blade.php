2e8825fb220103f44eb1d98b82219ddf0ebae987
taanmaster <web@taansystems.com>
2025-01-06 17:30:28 -0600

    Agregando Vistas

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('title')Intranet @endsection
+
+@section('content')
+    <!-- this is breadcrumbs -->
+    @component('components.breadcrumb')
+        @slot('li_1') Intranet @endslot
+        @slot('li_2') Usuarios @endslot
+        @slot('title') Perfil @endslot
+    @endcomponent
+
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Hola, {{ Auth::user()->name }}</h1>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+
+@endsection
