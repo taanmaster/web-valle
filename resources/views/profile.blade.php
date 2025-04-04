323bed9d5508962d7682d3643c274f87bfc546e9
taanmaster <web@taansystems.com>
2025-04-04 10:34:29 -0600

    actualizacion en vista perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.master')
 
 @section('title')
-    Intranet 
+    Intranet
 @endsection
 
 @section('content')
     <!-- this is breadcrumbs -->
     @component('components.breadcrumb')
-        @slot('li_1') Intranet @endslot
-        @slot('li_2') Usuarios @endslot
-        @slot('title') Perfil @endslot
+        @slot('li_1')
+            Intranet
+        @endslot
+        @slot('li_2')
+            Usuarios
+        @endslot
+        @slot('title')
+            Perfil
+        @endslot
     @endcomponent
 
     <div class="row">
@@ -20,5 +26,4 @@
 @endsection
 
 @section('script')
-
 @endsection
