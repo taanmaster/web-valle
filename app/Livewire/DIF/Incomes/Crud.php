e4b563c3c411f236900014949bd2cca6370aa4ac
taanmaster <web@taansystems.com>
2025-09-19 09:55:42 -0600

    CT3. DIF. Creacion de componentes

---
+++ app/Livewire/DIF/Incomes/Crud.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\DIF\Incomes;
+
+use Livewire\Component;
+
+class Crud extends Component
+{
+    public function render()
+    {
+        return view('livewire.d-i-f.incomes.crud');
+    }
+}
