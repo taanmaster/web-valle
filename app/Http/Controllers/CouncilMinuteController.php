d37b8d1a925b71bea950b45b30f1f2c272da30ca
taanmaster <web@taansystems.com>
2025-10-07 21:02:01 -0600

    CT6. Nuevos modelos para desarrollo mensual

---
+++ app/Http/Controllers/CouncilMinuteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CouncilMinute;
+use Illuminate\Http\Request;
+
+class CouncilMinuteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CouncilMinute $councilMinute)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CouncilMinute $councilMinute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CouncilMinute $councilMinute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CouncilMinute $councilMinute)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MunicipalInspectionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MunicipalInspection;
+use Illuminate\Http\Request;
+
+class MunicipalInspectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MunicipalInspection $municipalInspection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MunicipalInspection $municipalInspection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MunicipalInspection $municipalInspection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MunicipalInspection $municipalInspection)
+    {
+        //
+    }
+}
