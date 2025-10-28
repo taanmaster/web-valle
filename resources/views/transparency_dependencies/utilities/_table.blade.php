89745ff9a099d22ac560b3f7022279a74f3a96e9
Taan Systems <web@taansystems.com>
2025-10-28 17:59:58 -0600

    CT2. Reestructuración de roles CLAB Dependencias

--- resources/views/transparency_dependencies/utilities/_table.blade.php
+++ resources/views/transparency_dependencies/utilities/_table.blade.php
@@ -18,9 +18,9 @@
 
                 <div class="btn-group mt-3" role="group" aria-label="Basic example">
                     <a href="{{ route('transparency_dependencies.show', $dependency->id) }}" class="btn btn-sm btn-outline-primary"><i class='bx bx-show-alt'></i> Ver Detalle</a>
-                    <a href="{{ route('transparency_dependencies.edit', $dependency->id) }}" class="btn btn-sm btn-outline-secondary"><i class='bx bx-edit'></i> Editar</a>
                     
-                    @if (auth()->user()->hasRole('transparency_admin') || auth()->user()->hasRole('all')) 
+                    @if (auth()->user()->hasRole('financial_support') || auth()->user()->hasRole('transparency_admin') || auth()->user()->hasRole('all')) 
+                    <a href="{{ route('transparency_dependencies.edit', $dependency->id) }}" class="btn btn-sm btn-outline-secondary"><i class='bx bx-edit'></i> Editar</a>
                     <form method="POST" action="{{ route('transparency_dependencies.destroy', $dependency->id) }}" style="display: inline-block;">
                         <button type="submit" class="btn btn-sm btn-outline-danger">
                             <i class='bx bx-trash-alt text-danger'></i> Eliminar
