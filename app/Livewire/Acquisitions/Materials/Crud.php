2b9bd294c6ed9733e888632bebee9950484126ec
taanmaster <web@taansystems.com>
2025-11-23 22:13:23 -0600

    CT7. Crud de materiales

--- app/Livewire/Acquisitions/Materials/Crud.php
+++ app/Livewire/Acquisitions/Materials/Crud.php
@@ -4,8 +4,77 @@ namespace App\Livewire\Acquisitions\Materials;
 
 use Livewire\Component;
 
+// Ayudantes
+use Str;
+use Auth;
+use Session;
+use Carbon\Carbon;
+use Livewire\Attributes\Validate;
+use Livewire\Attributes\On;
+use Intervention\Image\Facades\Image as Image;
+use Livewire\WithFileUploads;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+
+use App\Models\AcquisitionMaterial;
+use App\Models\RegulatoryAgendaDependency;
 class Crud extends Component
 {
+    use WithFileUploads;
+
+    public $material;
+
+    //Modes: 0: create, 1 show, 2 edit
+    public $mode;
+
+    public $sku = '';
+    public $title = '';
+    public $is_active = '';
+
+    public $description = '';
+    public $category = '';
+    public $dependency_name = '';
+    public $current_stock = '';
+
+    public $dependencies = [];
+
+    public function mount()
+    {
+        $this->dependencies = RegulatoryAgendaDependency::get();
+
+        if ($this->material != null) {
+            $this->fetchMaterialData();
+        }
+    }
+
+    public function fetchMaterialData()
+    {
+        $this->title = $this->achievement->title;
+        $this->description = $this->achievement->description;
+        $this->image = $this->achievement->image;
+        $this->file = $this->achievement->file;
+        $this->hex = $this->achievement->hex;
+        $this->published_at = $this->achievement->published_at ? Carbon::parse($this->achievement->published_at)->format('Y-m-d') : null;
+        $this->is_active = $this->achievement->is_active;
+    }
+
+    public function save()
+    {
+        if ($this->material != null) {
+
+
+            Session::flash('success', 'El material se actualizo correctamente.');
+        } else {
+
+            Session::flash('success', 'El material se creo correctamente.');
+        }
+
+
+
+        return redirect()->route('acquisitions.materials.show', $this->material->id);
+
+    }
+
     public function render()
     {
         return view('acquisitions.materials.utilities.crud');
--- resources/views/acquisitions/materials/utilities/crud.blade.php
+++ resources/views/acquisitions/materials/utilities/crud.blade.php
@@ -1,53 +1,77 @@
 <div>
+    @if ($mode != 0)
+        <div class="row justify-content-end">
+            <div class="col-md-4">
 
-    <div class="row">
-        <div class="col-8">
-            <div class="card card-body">
-                <form method="POST" wire:submit="save" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label for="generic_name" class="form-label">Nombre genérico <span
+            </div>
+        </div>
+    @endif
+    <form method="POST" wire:submit="save" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <div class="row">
+            <div class="col-8">
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h4>Información</h4>
+                    </div>
+                    <div class="row card-body">
+                        <div class="col-md-12 mb-3">
+                            <label for="title" class="form-label">Nombre genérico <span
                                     class="text-danger tx-12">*</span></label>
-                            <input type="text" class="form-control" id="generic_name" name="generic_name" required>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="commercial_name" class="form-label">Nombre comercial <span
-                                    class="text-info tx-12">(Opcional)</span></label>
-                            <input type="text" class="form-control" id="commercial_name" name="commercial_name">
+                            <input type="text" class="form-control" id="title" name="title" required
+                                wire:model="title" @if ($mode == 1) disabled @endif>
                         </div>
 
                         <div class="col-md-12 mb-3">
                             <label for="description" class="form-label">Descripción <span
                                     class="text-info tx-12">(Opcional)</span></label>
-                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                            <textarea class="form-control" id="description" name="description" rows="3"
+                                @if ($mode == 1) disabled @endif wire:model="description"></textarea>
                         </div>
 
-                        <div class="col-md-12 mb-3">
-                            <label for="formula" class="form-label">Fórmula <span
-                                    class="text-info tx-12">(Opcional)</span></label>
-                            <textarea class="form-control" id="formula" name="formula" rows="2"></textarea>
+                    </div>
+                </div>
+                @if ($mode != 0)
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Inventario</h4>
                         </div>
+                        <div class="card-body">
 
-                        <div class="col-md-12 mb-3">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="is_active" name="is_active"
-                                    value="1" checked>
-                                <label class="form-check-label" for="is_active">Activo</label>
+                        </div>
+                    </div>
+                @endif
+            </div>
+            <div class="col-md-4">
+                @if ($mode != 0)
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h4>Estado</h4>
+                            <div class="card-body">
+                                <div class="col-md-12 mb-3">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="is_active" name="is_active"
+                                            value="1" checked>
+                                        <label class="form-check-label" for="is_active">Activo</label>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-dark btn-sm">Guardar Medicamento</button>
-                            <a href="{{ route('acquisitions.materials.index') }}"
-                                class="btn btn-secondary btn-sm">Cancelar</a>
-                        </div>
+                @endif
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Organización</h4>
+                        <div class="card-body"></div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
-
+        <div class="row">
+            <div class="col-12">
+                <button type="submit" class="btn btn-dark btn-sm">Guardar Medicamento</button>
+                <a href="{{ route('acquisitions.materials.index') }}" class="btn btn-secondary btn-sm">Cancelar</a>
+            </div>
+        </div>
+    </form>
 </div>
